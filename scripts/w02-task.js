87a47cffb25ca353f045a263a35443e3cab2e4c7
JaiYai1776 <sovereignscholars1776@gmail.com>
2024-01-16 19:37:17 -0700

    assignment changes

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,14 +3,14 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
-
+const fullName = "JeanAblbert Arnold";
+const currentYear = new Date().getFullYear();
+const profilePicture = "6images/self.jpg";
 
 /* Step 3 - Element Variables */
-
-
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const year = document.querySelector("#year");
 
 
 /* Step 4 - Adding Content */
