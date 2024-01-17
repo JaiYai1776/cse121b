0d96f70311dab4baa842b8ca3c5b3dd261407d70
JaiYai1776 <sovereignscholars1776@gmail.com>
2024-01-17 14:29:49 -0700

    Week 2 assignment

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -5,25 +5,28 @@
 /* Step 2 - Variables */
 const fullName = "JeanAblbert Arnold";
 const currentYear = new Date().getFullYear();
-const profilePicture = "6images/self.jpg";
+const profilePicture = "images/self.jpg";
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
-const year = document.querySelector("#year");
-
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
-
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `This is ${fullName}.  He is an online student at BYU-Idaho`);
 
 /* Step 5 - Array */
-
-
-
-
-
+let favFoods = ["Pizza", "Tacos", "Sushi", "Pasta", "Burgers"];
+foodElement.textContent = favFoods.join(', '); //This works, but is it correct?
+let newFavFood = "Ramen";
+favFoods.push(newFavFood);
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;
 
